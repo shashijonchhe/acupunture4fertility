6d63330667d6290ea12d2d0ff1b544d0eeb82532
shashijonchhe <48297213+shashijonchhe@users.noreply.github.com>
2020-07-21 11:35:31 -0700

    Update body.js

--- acupunture/src/body/body.js
+++ acupunture/src/body/body.js
@@ -4,12 +4,11 @@ import React from 'react';
 const Body=(props)=>{
     return(
         <div className="Trail">
-            <h1>WELCOME</h1>
+            <h1>Namaste</h1>
             <h2>We Turn Patients Into Parents</h2>    
             <h3>AACUPUNCTURE FOR FERTILITY: DR. SWASTI K SHRESTHA MSTCM, LAC</h3>
             <span>Acupuncture Fertility specialists is a cooperative effort of licensed acupuncturists specializing in women's and men's fertility and reproductive health, with offices in Sacramento and Roseville, California. We are dedicated to helping couples achieve and maintain a healthy pregnancy and ultimately deliver a healthy baby using Acupuncture and Herbal Medicine.</span>
-            <h1>USA</h1>
-            <h2>america</h2>
+            
         </div>
     )
 }
